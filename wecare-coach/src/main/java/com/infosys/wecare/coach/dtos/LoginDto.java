436a06dbeb3abe5669205ad56fc6638e54de2efd
niallduggangit <84376216+niallduggangit@users.noreply.github.com>
2022-09-02 14:47:44 +0100

    Delete wecare-coach directory

--- wecare-coach/src/main/java/com/infosys/wecare/coach/dtos/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.wecare.coach.dtos;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String id;
-    private String password;
-}
--- wecare-coach/src/main/java/com/infosys/wecare/coach/repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.wecare.coach.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.wecare.coach.entities.Coach;
-
-public interface CoachRepository extends JpaRepository<Coach, String> {
-}
