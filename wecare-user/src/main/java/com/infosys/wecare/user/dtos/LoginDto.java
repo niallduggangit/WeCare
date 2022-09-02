95c366f58170ab71b5d33c0de48144abc88a11d7
niallduggangit <84376216+niallduggangit@users.noreply.github.com>
2022-09-02 14:48:34 +0100

    Delete wecare-user directory

--- wecare-user/src/main/java/com/infosys/wecare/user/dtos/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.wecare.user.dtos;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String id;
-    private String password;
-}
--- wecare-user/src/main/java/com/infosys/wecare/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.wecare.user.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.wecare.user.entities.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
